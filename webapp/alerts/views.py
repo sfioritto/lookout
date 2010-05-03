4e9ce7f2c6d30638e3bdb27c14ff7cf525d47f15
Sean Fioritto <seanfioritto@sean-fiorittos-macbook-air.local>
2010-05-02 21:11:16 -0500

    Added blank template. Created stub views.

--- webapp/alerts/views.py
+++ webapp/alerts/views.py
@@ -1,1 +1,25 @@
-# Create your views here.
+
+
+def create_alert(request):
+
+    """
+    """
+
+    if request.method == "POST":
+        form = RequestForm(request.POST)
+        email = form.data['email']
+        return HttpResponseRedirect(reverse("webapp.alerts.show_all"))
+    else:
+        return render_to_response('marketing/create-alert.html',
+                                  context_instance=RequestContext(request))
+
+
+def show_all(request):
+
+    """
+    """
+
+    
+
+
+
