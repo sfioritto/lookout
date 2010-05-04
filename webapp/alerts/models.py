2316f6906ccf844be8f69b38e03f38af20d2801c
Sean Fioritto <seanfioritto@sean-fiorittos-macbook-air.local>
2010-05-03 19:50:23 -0500

    Added models and created django apps.

--- webapp/alerts/models.py
+++ webapp/alerts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from webapp.account.models import User
+from webapp.folders.models import Folder
 
-# Create your models here.
+class Alert(models.Model):
+
+    created_on = models.DateTimeField(auto_now_add=True, auto_now=True)
+    user = models.ForeignKey(User)
+    folder = models.ForeignKey(Folder)
+    term = models.CharField(max_length=256)
+    type = models.CharField(max_length=256)
+    email = models.CharField(max_length=512)
+    frequency = models.CharField(max_length=256)
+    length = models.IntegerField()
+
+
+    
