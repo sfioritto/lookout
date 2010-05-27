e72c18a709daf402f98ec96ca139043682a55af7
Sean Fioritto <seanfioritto@sean-fiorittos-macbook-air.local>
2010-05-26 21:06:16 -0500

    Added webapp settings.

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,12 @@
 from fabric.api import *
-from datetime import datetime
 
-env.hosts = ['postosaurus.com']
-env.approot = "/var/local/postosaurus"
+env.hosts = ['lookoutthere.com']
+env.approot = "/var/local/www"
+env.prodhome = "/var/local/"
 
 
 def test():
-    local("nosetests")
+    local("nosetests tests/")
 
 
 def pack(hash):
@@ -36,9 +36,9 @@ def upload(archive):
 def untar(archive, hash):
     with settings(warn_only=True):
         with cd(env.prodhome):
-            run("rm -rf snapshots/%s" % hash)
-            run("mkdir snapshots/%s" % hash)
-            run("cd snapshots/%s; tar -xvf '/tmp/%s'" % (hash,archive))
+            sudo("rm -rf snapshots/%s" % hash)
+            sudo("mkdir snapshots/%s" % hash)
+            sudo("cd snapshots/%s; tar -xvf '/tmp/%s'" % (hash,archive))
 
 
 def upload_untar(archive, hash):
@@ -48,13 +48,11 @@ def upload_untar(archive, hash):
 
 def switch(hash):
     with cd(env.prodhome):
-        sudo("ln -s %s/snapshots/%s/app /tmp/live_tmp && sudo mv -Tf /tmp/live_tmp /var/local/postosaurus/app" % (env.prodhome, hash))
-        sudo("ln -s %s/snapshots/%s/webapp /tmp/live_tmp && sudo mv -Tf /tmp/live_tmp /var/local/postosaurus/webapp" % (env.prodhome, hash))
-        sudo("ln -s %s/snapshots/%s/media /tmp/live_tmp && sudo mv -Tf /tmp/live_tmp /var/local/postosaurus/media" % (env.prodhome, hash))
-        sudo("ln -s %s/snapshots/%s/config /tmp/live_tmp && sudo mv -Tf /tmp/live_tmp /var/local/postosaurus/config" % (env.prodhome, hash))
-    with cd(env.approot):
-        run("cp webappsettings.py webapp/settings.py")
-        run("cp config/prod-settings.py config/settings.py")
+        sudo("ln -s %s/snapshots/%s /tmp/live_tmp && sudo mv -Tf /tmp/live_tmp /var/local/lookout" % (env.prodhome, hash))
+
+#     with cd(env.approot):
+#         run("cp webappsettings.py webapp/settings.py")
+#         run("cp config/prod-settings.py config/settings.py")
 
 
 def reboot():
@@ -66,38 +64,16 @@ def reboot():
             sudo("lamson start -gid 1000 -uid 1000")
             sudo("lamson start -gid 1000 -uid 1000 -boot config.queue -pid run/queue.pid")
             sudo("chown -R %s:%s ../postosaurus" % (env.user, env.user))
+
     
-
-def image(instance):
-    date = datetime.today().strftime("%Y%m%d%H%M")
-    bucket = 'postosaurus' + date
-
-    put('~/.ec2/%s' % env.aws_private_key, '/tmp/%s' % env.aws_private_key)
-    put('~/.ec2/%s' % env.aws_certificate, '/tmp/%s' % env.aws_certificate)
-    sudo('mv /tmp/%s /mnt/%s' % (env.aws_private_key, env.aws_private_key))
-    sudo('mv /tmp/%s /mnt/%s' % (env.aws_certificate, env.aws_certificate))
-    
-    #bundle
-    sudo('ec2-bundle-vol --no-inherit -d /mnt -k /mnt/%s -c /mnt/%s -u %s -r i386' % (env.aws_private_key, env.aws_certificate, env.aws_user))
-
-    #updload
-    sudo('ec2-upload-bundle -b %s -m /mnt/image.manifest.xml -a %s -s %s %s' % (bucket, env.aws_access_key, env.aws_secret_key, instance))
-
-    #register
-    local("ec2-register -K ~/.ec2/%s -C ~/.ec2/%s %s/image.manifest.xml" % (env.aws_private_key, env.aws_certificate, bucket))
-
-    cleanup()
-
-def cleanup():
-    sudo("rm /mnt/image*; rm -rf /mnt/img-mnt/; rm /mnt/%s; rm /mnt/%s" % (env.aws_private_key, env.aws_certificate))    
-
 def deploy(hash):
-    with cd(env.devpath):
-        test()
-        archive = pack(hash)
-    upload_untar(archive, hash)
-    switch(hash)
-    reboot()
+    
+    test()
+    archive = pack(hash)
+    upload(archive)
+    untar(archive, hash)
+#    switch(hash)
+#    reboot()
 
     
     
