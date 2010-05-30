16948d6466395c29fc14ae69c4249bd8ec354911
Sean Fioritto <seanfioritto@sean-fiorittos-macbook-air.local>
2010-05-30 10:14:11 -0500

    Added logging for production debugging.

--- emails/app/handlers/alerts.py
+++ emails/app/handlers/alerts.py
@@ -1,9 +1,11 @@
 import app.model.alerts as alerts
+import logging
 from lamson import queue
 from lamson.routing import route, route_like, state_key_generator
 from email.utils import parseaddr
 from webapp.alerts.models import Alert
 
+LOG = logging.getLogger("alerts")
 
 @state_key_generator
 def module_and_listname(modulename, message):
@@ -28,14 +30,16 @@ def CONFIRMING(message, alert_id=None, host=None):
         # google alerts sends alerts from a different address than the confirmation email. Lamson
         # state key includes the sender, so I have to add this extra piece.
         if alert.confirmed:
+            LOG.debug("Alert for alert id %s was already confirmed." % alert_id)
             return ALERTING(message, alert_id=alert_id, host=host)
         alerts.confirm_alert(message)
         alert.confirmed = True
         alert.save()
         return ALERTING
-    except:
+    except Exception as e:
         q = queue.Queue('run/error')
         q.push(message)
+        LOG.debug(str(e))
         return CONFIRMING
 
 
