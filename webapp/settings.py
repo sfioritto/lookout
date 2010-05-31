50da74ad4d8f72faec9a20a0d78f55cfb93743ea
Sean Fioritto <seanfioritto@sean-fiorittos-macbook-air.local>
2010-05-31 17:23:13 -0500

    Added thumbs down image and hover.

--- webapp/settings.py
+++ webapp/settings.py
@@ -42,7 +42,7 @@ MEDIA_ROOT = '/Users/seanfioritto/lookout/media'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/site_media/'
+MEDIA_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
