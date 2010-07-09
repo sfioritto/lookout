92c5fb418831d504fb2df6de914d7a75cb6452a9
Sean Fioritto <seanfioritto@sean-fiorittos-macbook-air.local>
2010-07-08 21:36:29 -0500

    Finished markup and javascript for updating configuration of the feed.

--- media/javascript/feed/feed.js
+++ media/javascript/feed/feed.js
@@ -1,34 +1,51 @@
-$(document).ready(function(){
+$(document).ready(
+function(){
 
 
-		      /* go to the visit url on the hidden anchor tag instead of the href on the event target*/
-		      $("a.blurb, a.textbtn").click(function(event){
-							var container = $(event.currentTarget).parents("li.blurb");
-							var url = $("a.visit", container)[0].href;
-							var oldurl = event.currentTarget.href;
-							event.currentTarget.href = url;
-							$(container).addClass("visited");
-							setTimeout(function(){
-								       event.currentTarget.href = oldurl;
-								   }, 0);
-					 });
+    $("select.relfilter").change(
+    function(event){
+	var select = $(event.target).hide();
+	var text = $("select.relfilter :selected").text();
+	$("a.relfilter").html(text).show();
+    });
 
-		      /*mark the div as irrelevant and post to the relevance view*/
-		      $("a.relevance").click(function(event){
+    $("a.relfilter").click(
+    function(event){
+	$(event.target).hide();
+	$("select.relfilter").show();
+	event.preventDefault();
+    });
 
-						 var relevance = "false",
-						 blurb = $(event.target).parent().parent();
+    /* go to the visit url on the hidden anchor tag instead of the href on the event target*/
+    $("a.blurb, a.textbtn").click(
+    function(event){
+	var container = $(event.currentTarget).parents("li.blurb");
+	var url = $("a.visit", container)[0].href;
+	var oldurl = event.currentTarget.href;
+	event.currentTarget.href = url;
+	$(container).addClass("visited");
+	setTimeout(function(){
+		       event.currentTarget.href = oldurl;
+		   }, 0);
+    });
 
-						 if (blurb.hasClass("irrelevant")){
-						     relevance = "true";
-						     blurb.removeClass("irrelevant");
-						 } else {
-						     blurb.addClass("irrelevant");
-						 }
+    /*mark the div as irrelevant and post to the relevance view*/
+    $("a.relevance").click(
+    function(event){
 
-						  $.post(event.target.href, {
-							     'relevance' : relevance
-						  });
-						  event.preventDefault();
-					      });
+	var relevance = "false",
+	blurb = $(event.target).parent().parent();
+
+	if (blurb.hasClass("irrelevant")){
+	    relevance = "true";
+	    blurb.removeClass("irrelevant");
+	} else {
+	    blurb.addClass("irrelevant");
+	}
+
+	$.post(event.target.href, {
+		   'relevance' : relevance
+	       });
+	event.preventDefault();
+    });
 });
