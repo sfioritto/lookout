07c7f32f45a0ed9d5bee0936f4add295ca480023
Sean Fioritto <seanfioritto@sean-fiorittos-macbook-air.local>
2010-07-18 16:19:52 -0500

    Mostly fixed inconsistent left padding across pages.

--- webapp/account/urls.py
+++ webapp/account/urls.py
@@ -12,8 +12,12 @@ urlpatterns += patterns(
         'template_name': 'account/login.html'}
      ),
     (r'logout/$', 'logout_then_login'),
-    (r'^registration/change/$', 'password_change', {'template_name' : 'account/registration/password_change_form.html'}),
-    (r'^registration/change/done/$', 'password_change_done'),
+    (r'^registration/change/$', 'password_change', {
+            'template_name' : 'account/registration/password_change_form.html'
+            }),
+    (r'^registration/change/done/$', 'password_change_done', {
+            'template_name' : 'account/registration/password_change_done.html'
+            }),
     (r'^registration/reset/$', 'password_reset'),
     (r'^registration/reset/done/$', 'password_reset_done'),
     (r'^registration/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm'),
