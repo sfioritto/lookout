d714b98c8b4c99c19bfc0abbf4bf4e89c7009992
Sean Fioritto <seanfioritto@sean-fiorittos-macbook-air.local>
2010-07-18 17:20:12 -0500

    Completed all basic account screens except the actual profile screen.

--- webapp/account/urls.py
+++ webapp/account/urls.py
@@ -18,9 +18,17 @@ urlpatterns += patterns(
     (r'^registration/change/done/$', 'password_change_done', {
             'template_name' : 'account/registration/password_change_done.html'
             }),
-    (r'^registration/reset/$', 'password_reset'),
-    (r'^registration/reset/done/$', 'password_reset_done'),
-    (r'^registration/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm'),
-    (r'^registration/reset/done/$', 'password_reset_complete'),
+    (r'^registration/reset/$', 'password_reset', {
+            'template_name' : 'account/registration/password_reset_form.html'
+            }),
+    (r'^registration/reset/done/$', 'password_reset_done', {
+            'template_name' : 'account/registration/password_reset_done.html'
+            }),
+    (r'^registration/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm', {
+            'template_name' : 'account/registration/password_reset_confirm.html'
+            }),
+    (r'^registration/reset/complete/$', 'password_reset_complete', {
+            'template_name' : 'account/registration/password_reset_complete.html'
+            }),
     )
 
